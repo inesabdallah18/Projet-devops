67fb4655ca19340e9428f1fb6502a0748830f1bc
Ines Abdallah <inesab149@gmail.com>
2026-02-25 01:44:10 +0100

    Ajout application.properties dans test pour forcer H2 et éviter MySQL en tests

--- src/test/java/tn/esprit/tpfoyer17/controllers/ChambreControllerTest.java
+++ src/test/java/tn/esprit/tpfoyer17/controllers/ChambreControllerTest.java
@@ -1,6 +1,5 @@
 package tn.esprit.tpfoyer17.controllers;
 
-import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +8,6 @@ import tn.esprit.tpfoyer17.entities.Chambre;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class ChambreControllerTest {
 
     @Autowired
@@ -17,24 +15,15 @@ class ChambreControllerTest {
 
     @Test
     void testAddChambre_simple() {
-        // Création d'une chambre très simple
         Chambre chambre = new Chambre();
-        chambre.setNumeroChambre(777L); // un numéro arbitraire
-
-        // Appel de la méthode du controller
+        chambre.setNumeroChambre(777L);
         Chambre resultat = chambreController.addChambre(chambre);
-
-        // Vérification ultra-basique : on s'assure que ça renvoie quelque chose
         assertThat(resultat).isNotNull();
     }
 
     @Test
     void testRetrieveChambre_simple() {
-        // On essaie de récupérer une chambre (avec un ID qui existe ou pas, peu importe ici)
         Chambre resultat = chambreController.retrieveChambre(1L);
-
-        // Vérification minimale : on vérifie juste que ça ne renvoie pas null
-        // (même si ça plante sur la base de données, JaCoCo comptera quand même les lignes appelées)
-        assertThat(resultat).isNotNull(); // ou retire cette ligne si ça plante
+        assertThat(resultat).isNotNull();
     }
 }
