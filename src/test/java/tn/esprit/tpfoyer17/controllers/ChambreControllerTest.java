d9d66b0c3fa6d6b397570b4cc5c44c5d492914a5
Ines Abdallah <inesab149@gmail.com>
2026-02-25 01:50:47 +0100

    testproperty

--- src/test/java/tn/esprit/tpfoyer17/controllers/ChambreControllerTest.java
+++ src/test/java/tn/esprit/tpfoyer17/controllers/ChambreControllerTest.java
@@ -3,11 +3,13 @@ package tn.esprit.tpfoyer17.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;  // ← AJOUTE CET IMPORT
 import tn.esprit.tpfoyer17.entities.Chambre;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")  // ← AJOUTE CETTE LIGNE
 class ChambreControllerTest {
 
     @Autowired
