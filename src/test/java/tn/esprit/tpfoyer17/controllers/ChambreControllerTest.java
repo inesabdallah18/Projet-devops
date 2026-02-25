02d99ffacac74f1600099ce51fb6191932d7aedc
Ines Abdallah <inesab149@gmail.com>
2026-02-25 01:55:21 +0100

    test inline

--- src/test/java/tn/esprit/tpfoyer17/controllers/ChambreControllerTest.java
+++ src/test/java/tn/esprit/tpfoyer17/controllers/ChambreControllerTest.java
@@ -3,13 +3,22 @@ package tn.esprit.tpfoyer17.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;  // ← AJOUTE CET IMPORT
 import tn.esprit.tpfoyer17.entities.Chambre;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@TestPropertySource(locations = "classpath:application.properties")  // ← AJOUTE CETTE LIGNE
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=MySQL",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=update",
+    "spring.flyway.enabled=false",
+    "spring.liquibase.enabled=false",
+    "spring.jpa.show-sql=true",
+    "spring.jpa.properties.hibernate.format_sql=true"
+})
 class ChambreControllerTest {
 
     @Autowired
